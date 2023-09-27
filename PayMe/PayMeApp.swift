d8e8583898b6b2a9c353ea650ab5c784884bd7cc
KDatta97 <kaviddazun97@gmail.com>
2023-09-27 13:55:56 +0800

    Initial Commit

---
+++ PayMe/PayMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayMeApp.swift
+//  PayMe
+//
+//  Created by user244521 on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PayMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
